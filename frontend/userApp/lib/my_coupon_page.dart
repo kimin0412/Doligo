56d6446e6c201cdb6ba942e72294aa87cd0d59fa
함지훈 <logbug1004@gmail.com>
2020-10-28 14:52:18 +0900

    [ADD, UPDATE] make my coupon page & button listener(S03P31A401-74)

---
+++ frontend/userApp/lib/my_coupon_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyCouponPage extends StatefulWidget {
+  @override
+  _MyCouponPageState createState() => _MyCouponPageState();
+}
+
+class _MyCouponPageState extends State<MyCouponPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
