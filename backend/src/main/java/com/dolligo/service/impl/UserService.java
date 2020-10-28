a79942c9f03e45d0a35d968eb2bcb87c071efefd
kjy951207 <kjy951207@naver.com>
2020-10-28 22:04:54 +0900

    [ADD] user api

--- backend/src/main/java/com/dolligo/service/impl/UserService.java
+++ backend/src/main/java/com/dolligo/service/impl/UserService.java
@@ -1,5 +1,6 @@
 package com.dolligo.service.impl;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.mail.MessagingException;
@@ -11,6 +12,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import com.dolligo.dao.PreferenceDao;
+import com.dolligo.dto.Preference;
 import com.dolligo.dto.User;
 import com.dolligo.exception.ApplicationException;
 import com.dolligo.exception.NotFoundException;
@@ -39,12 +41,11 @@ public class UserService implements IUserService {
     @Override
     public User getMyInfo(int id) throws Exception{
     	Optional<User> user = this.userRepository.findById(id);
+
     	if(!user.isPresent()) {
     		throw new NotFoundException("회원 정보 찾지 못함");
     	}
     	User me = user.get(); 
-//    	me.setPreferences(preferenceDao.findByUid(id));
-    	me.setPassword("");
 	    
 	    return me;
     }
@@ -57,128 +58,93 @@ public class UserService implements IUserService {
     		throw new NotFoundException("회원 정보 찾지 못함");
     	}
     	
-//    	user.setPreferences(preferenceDao.findByUid(user.getId()));
-    	user.setPassword("");
-    	
     	return user;
     }
 
 
+    //회원가입
+    @Override
+    public User add(User user) throws Exception{
+        user.setPassword(SHA256.testSHA256(user.getPassword()));
+        this.userRepository.save(user);
+
+        List<Preference> plist = user.getPreferences();
+        
+        for(Preference p : plist) {
+        	p.setUid(user.getId());
+        	preferenceDao.save(p);
+        }
+        
+        return user;
+    }
+
+    //회원 수정
+    @Override
+    public User update(User user) throws Exception{
+        String password = this.userRepository.selectPassword(user.getId());
+        
+        if(password == null || password.equals("")) {
+        	throw new ApplicationException("회원 정보를 찾을 수 없습니다.");
+        }
+
+        //비번 변경인 경우
+        if(user.getPassword() == null || user.getPassword().equals(""))
+            user.setPassword(password);
+        else user.setPassword(SHA256.testSHA256(user.getPassword()));
+        
+
+        this.userRepository.save(user);
+        List<Preference> plist = user.getPreferences();
+        
+        for(Preference p : plist) {
+        	preferenceDao.save(p);
+        }
+
+        user.setPassword("");
+        return user;
+    }
+
+    @Override
+    public void delete(String id) throws Exception {
+        this.userRepository.deleteById(Integer.parseInt(id));
+    }
+
+
+    //이메일 중복확인
 	@Override
-	public User add(User user) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public boolean isDupEmail(String email) throws Exception{
+		return userRepository.isDupEmail(email) > 0 ? true : false;
+	}
+
+
+	//본인확인
+	@Override
+	public boolean checkPassword(String uid, String password) throws Exception{
+		return userRepository.checkPassword(uid, password) > 0 ? true : false;
 	}
 
 
 	@Override
-	public User update(User user) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	@Override
-	public void delete(String uid) throws Exception {
-		// TODO Auto-generated method stub
+	public void sendTmpPasswordEmail(String password, String email) throws Exception{
+		String title = "Dolligo 임시 비밀번호 발급";
+		String content = "\n\n안녕하세요!, 임시 비밀번호로 로그인 후 반드시 수정해주세요!!"
+						+ "\n\n새 비밀번호 : " + password; // 내용
+            
+		userRepository.updatePasswordByEmail(email, SHA256.testSHA256(password));
 		
+		try {
+			MimeMessage message = mailSender.createMimeMessage();
+	        MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+	        messageHelper.setTo(email); // 받는사람 이메일
+	        messageHelper.setSubject(title); // 메일제목
+	        messageHelper.setText(content); // 메일 내용
+	        
+	        mailSender.send(message);
+		}catch(MessagingException e) {
+			e.printStackTrace();
+			throw new ApplicationException(e.getMessage());
+		}
 	}
-
-
-	@Override
-	public boolean isDupEmail(String email) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-
-	@Override
-	public boolean checkPassword(String uid, String password) throws Exception {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-
-	@Override
-	public void sendTmpPasswordEmail(String password, String email) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
-//    //회원가입
-//    @Override
-//    public User add(User user) throws Exception{
-//        user.setPassword(SHA256.testSHA256(user.getPassword()));
-//        this.userRepository.create(user);
-//        
-//        return user;
-//    }
-//
-//    //회원 수정
-//    @Override
-//    public User update(User user) throws Exception{
-//
-//        Optional<User> result = this.userRepository.findById(user.getId());
-//        
-////        this.userRepository.save
-//        User found = result.get();
-//        if(found == null)
-//            throw new ApplicationException("회원 정보를 찾을 수 없습니다.");
-//        
-//        if(user.getPassword() == null || user.getPassword().equals(""))
-//            user.setPassword(found.getPassword());
-//        else user.setPassword(SHA256.testSHA256(user.getPassword()));
-//        
-//        if(user.getEmail() == null || user.getEmail().equals(""))
-//        	user.setEmail(found.getEmail());
-//
-//        int affected = this.userRepository.update(user);
-//        if(affected == 0)
-//            throw new ApplicationException("회원수정 처리가 반영되지 않았습니다.");
-//
-//        return this.userRepository.findById(user.getId()).get();
-//    }
-//
-//    @Override
-//    public void delete(String id) throws Exception {
-//        this.userRepository.delete(id);
-//    }
-//
-//
-//    //이메일 중복확인
-//	@Override
-//	public boolean isDupEmail(String email) throws Exception{
-//		return userRepository.isDupEmail(email) > 0 ? true : false;
-//	}
-//
-//
-//	@Override
-//	public boolean checkPassword(String uid, String password) throws Exception{
-//		return userRepository.checkPassword(uid, password) > 0 ? true : false;
-//	}
-//
-//
-//	@Override
-//	public void sendTmpPasswordEmail(String password, String email) throws Exception{
-//		String title = "Dolligo 임시 비밀번호 발급";
-//		String content = "\n\n안녕하세요!, 임시 비밀번호로 로그인 후 반드시 수정해주세요!!"
-//						+ "\n\n새 비밀번호 : " + password; // 내용
-//            
-//		userRepository.updatePasswordByEmail(email, SHA256.testSHA256(password));
-//		
-//		try {
-//			MimeMessage message = mailSender.createMimeMessage();
-//	        MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
-//	        messageHelper.setTo(email); // 받는사람 이메일
-//	        messageHelper.setSubject(title); // 메일제목
-//	        messageHelper.setText(content); // 메일 내용
-//	        
-//	        mailSender.send(message);
-//		}catch(MessagingException e) {
-//			e.printStackTrace();
-//			throw new ApplicationException(e.getMessage());
-//		}
-//	}
 
 
 
