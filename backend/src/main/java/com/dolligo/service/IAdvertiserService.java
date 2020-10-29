37d480a43a4043196e16766ad0c9e0b220fb8fcb
김민지 <kimin0412@gmail.com>
2020-10-29 15:29:11 +0900

    [UPDATE] Advertiser Feature

--- backend/src/main/java/com/dolligo/service/IAdvertiserService.java
+++ backend/src/main/java/com/dolligo/service/IAdvertiserService.java
@@ -2,8 +2,25 @@ package com.dolligo.service;
 
 import org.springframework.transaction.annotation.Transactional;
 
-import com.dolligo.dto.User;
+import com.dolligo.dto.Advertiser;
 
 public interface IAdvertiserService {
+	@Transactional
+	Advertiser getMyInfo(int id) throws Exception;//내 정보
+	@Transactional
+	Advertiser getAdvertiserInfo(String email) throws Exception;
+    @Transactional
+    Advertiser add(Advertiser advertiser) throws Exception;//회원가입
+    @Transactional
+    Advertiser update(Advertiser advertiser) throws Exception;//회원수정
+    
+    @Transactional
+    void delete(String uid) throws Exception;//회원탈퇴
+    
+	boolean isDupEmail(String email) throws Exception;
+	
+	//비번 확인
+	public boolean checkPassword(String uid, String password) throws Exception;
+	public void sendTmpPasswordEmail(String password, String email) throws Exception;
 
 }
