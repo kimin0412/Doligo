31f2bed65d68c1976e1a817b03ce7323001b24bc
kjy951207 <kjy951207@naver.com>
2020-11-02 13:06:51 +0900

    [UPDATE] coupon api

--- backend/src/main/java/com/dolligo/repository/ICouponRepository.java
+++ backend/src/main/java/com/dolligo/repository/ICouponRepository.java
@@ -14,8 +14,8 @@ public interface ICouponRepository extends JpaRepository<Coupon, Integer>{
 	@Query(value = "select * from coupon where uid = ?1 and pid = ?2 ", nativeQuery = true)
 	Coupon findByPidAndUid(String uid, int pid);
 
-	//쿠폰 기한 30일로 해둠
-	@Query(value = "select * from coupon where uid = ?1 and timestampdiff(day, created, now()) < 31 ", nativeQuery = true)
+	//쿠폰 기한 30일로 해둠( and timestampdiff(day, created, now()) < 31)
+	@Query(value = "select * from coupon where uid = ?1 and used = false ", nativeQuery = true)
 	List<Coupon> findAllByUid(String uid);
 	
     
