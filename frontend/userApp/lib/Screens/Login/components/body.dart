1846ed0ee759556e24bd30ed7191372946f8516c
함지훈 <logbug1004@gmail.com>
2020-11-03 11:14:52 +0900

    [UPDATE] http connect(backend-frontend)

--- frontend/userApp/lib/Screens/Login/components/body.dart
+++ frontend/userApp/lib/Screens/Login/components/body.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart';
 import 'package:userApp/Screens/Login/components/background.dart';
 import 'package:userApp/Screens/Signup/signup_screen.dart';
 import 'package:userApp/components/already_have_an_account_acheck.dart';
@@ -8,62 +13,134 @@ import 'package:userApp/components/rounded_password_field.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:userApp/home_page.dart';
 import 'package:userApp/tap_page.dart';
+import 'package:http/http.dart' as http;
 
-class Body extends StatelessWidget {
-  const Body({
-    Key key,
-  }) : super(key: key);
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  String _email;
+  String _password;
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Background(
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "LOGIN",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: size.height * 0.03),
-            SvgPicture.asset(
-              //"assets/icons/login.svg",
-              "assets/icons/logo_4.svg",
-              height: size.height * 0.35,
-            ),
-            SizedBox(height: size.height * 0.03),
-            RoundedInputField(
-              hintText: "이메일을 입력해주세요.",
-              onChanged: (value) {},
-            ),
-            RoundedPasswordField(
-              onChanged: (value) {},
-            ),
-            RoundedButton(
-              text: "LOGIN",
-              press: () {
-                Navigator.pushAndRemoveUntil(context,
-                    MaterialPageRoute(builder: (BuildContext context) => TabPage()), (route) => false
-                );
-              },
-            ),
-            SizedBox(height: size.height * 0.03),
-            AlreadyHaveAnAccountCheck(
-              press: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return SignUpScreen();
-                    },
-                  ),
-                );
-              },
-            ),
-          ],
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Background(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "LOGIN",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: size.height * 0.03),
+              SvgPicture.asset(
+                //"assets/icons/login.svg",
+                "assets/icons/logo_4.svg",
+                height: size.height * 0.3,
+              ),
+              SizedBox(height: size.height * 0.03),
+              RoundedInputField(
+                hintText: "이메일을 입력해주세요.",
+                onChanged: (value) {
+                  _email = value;
+                },
+              ),
+              RoundedPasswordField(
+                onChanged: (value) {
+                  _password = value;
+                },
+              ),
+              RoundedButton(
+                text: "LOGIN",
+                press: () {
+                  // 로그인 하기 위해 이메일과 비밀번호를 추출
+                  // print('email : $_email');
+                  // print('password : $_password');
+
+                  final responseCode = signIn();
+
+                },
+              ),
+              SizedBox(height: size.height * 0.03),
+              AlreadyHaveAnAccountCheck(
+                press: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return SignUpScreen();
+                      },
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+
+  Future<int> signIn() async {
+    final response = await http.post('http://k3a401.p.ssafy.io:8080/api/user/signin',
+      body: jsonEncode(
+          {
+            'email' : _email,
+            'password' : _password
+          }
+      ),
+      headers: {'Content-Type' : "application/json"},
+    );
+
+    final body = json.decode(response.body);
+    final header = response.headers['token'];
+
+    print('response : $body');
+    print('response : $header');
+
+
+    if(response.statusCode == 200 || response.statusCode == 202) {
+      FlutterSecureStorage().write(key: 'token', value: response.headers['token']); // 토큰 저장
+    }
+
+    final responseCode = response.statusCode;
+    if(responseCode == 202 || responseCode == 200) {
+      Navigator.pushAndRemoveUntil(context,
+          MaterialPageRoute(builder: (BuildContext context) => TabPage()), (route) => false
+      );
+    } else if(responseCode == 400) {
+      Fluttertoast.showToast(
+          msg: "비밀번호가 일치하지 않습니다",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0
+      );
+    } else {
+      Fluttertoast.showToast(
+          msg: "해당 아이디가 존재하지 않습니다",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0
+      );
+    }
+
+    return response.statusCode;
+  }
+
+
 }
+
