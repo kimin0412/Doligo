2f13c7801e5dd3c5e1c58c482dfb628062bfae83
함지훈 <logbug1004@gmail.com>
2020-11-08 01:30:49 +0900

    [UPDATE] Add gender & age picker

--- frontend/userApp/lib/private_info_setting_page.dart
+++ frontend/userApp/lib/private_info_setting_page.dart
@@ -4,7 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
+import 'Screens/Signup/signup_screen_2.dart';
+import 'constants.dart';
 import 'main.dart';
 
 class PrivateInfoSettingPage extends StatefulWidget {
@@ -23,6 +26,20 @@ class _PrivateInfoSettingPageState extends State<PrivateInfoSettingPage> {
   String _nickname;
   String _password1, _password2, _password3;
 
+  List<Gender> genders = new List<Gender>();
+
+  bool _isFemale;
+
+  int _selectedYear;
+
+  List<int> userYears = <int>[
+    1960, 1961, 1962, 1963, 1964, 1965, 1966, 1967, 1968, 1969,
+    1970, 1971, 1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979,
+    1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989,
+    1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999,
+    2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009
+  ];
+
   @override
   void initState() {
     // TODO: implement initState
@@ -31,6 +48,10 @@ class _PrivateInfoSettingPageState extends State<PrivateInfoSettingPage> {
     isChanged = false;
 
     _getUserInfo();
+
+    genders.add(new Gender("Male", MdiIcons.genderMale, false));
+    genders.add(new Gender("Female", MdiIcons.genderFemale, false));
+    genders.add(new Gender("Others", MdiIcons.genderTransgender, false));
   }
 
   @override
@@ -163,7 +184,73 @@ class _PrivateInfoSettingPageState extends State<PrivateInfoSettingPage> {
                       return value.contains('@') ? 'Do not use the @ char.' : null;
                     },
                   ),
-                  SizedBox(height: 50,),
+                  SizedBox(height: 15,),
+                  Container(
+                      height: 100,
+                      width: 600,
+                      alignment: Alignment.center,
+                      child:
+                      ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          itemCount: genders.length,
+                          itemBuilder: (context, index) {
+                            return InkWell(
+                              splashColor: kPrimaryColor,
+                              onTap: () {
+                                setState(() {
+                                  if(index == 2) {
+                                    Fluttertoast.showToast(
+                                        msg: '당신은 사람이 아닙니까?',
+                                        toastLength: Toast.LENGTH_SHORT,
+                                        gravity: ToastGravity.BOTTOM,
+                                        timeInSecForIosWeb: 1,
+                                        backgroundColor: Colors.grey,
+                                        textColor: Colors.white,
+                                        fontSize: 16.0
+                                    );
+                                  } else {
+                                    genders.forEach((gender) => gender.isSelected = false);
+                                    genders[index].isSelected = true;
+                                    if(index == 0) _isFemale = false;      // 남자면 false
+                                    else if(index == 1) _isFemale = true;  // 여자면 true
+                                  }
+                                });
+                              },
+                              child: CustomRadio(genders[index]),
+                            );
+                          }
+                      )
+                  ),  // gender selector
+                  Container(
+                      height: 100,
+                      width: 600,
+                      alignment: Alignment.center,
+                      child:
+                      DropdownButton<int>(
+                        hint:  Text("출생년도를 골라주세요."),
+                        value: _selectedYear,
+                        onChanged: (int Value) {
+                          setState(() {
+                            _selectedYear = Value;
+                          });
+                        },
+                        items: userYears.map((int year) {
+                          return  DropdownMenuItem<int>(
+                            value: year,
+                            child: Row(
+                              children: <Widget>[
+                                SizedBox(width: 10,),
+                                Text(
+                                  year.toString(),
+                                  style:  TextStyle(color: Colors.black),
+                                ),
+                              ],
+                            ),
+                          );
+                        }).toList(),
+                      )), // year choice
+                  SizedBox(height: 15,),
                   Container(
                     child: Text('비밀번호 변경'),
                     alignment: Alignment.centerLeft,
@@ -229,6 +316,8 @@ class _PrivateInfoSettingPageState extends State<PrivateInfoSettingPage> {
       _userInfo = json.decode(response.body)['data'];
       print('userInfo : $_userInfo');
       _nickname = _userInfo['nickname'];
+      genders[_userInfo['gender'] ? 1 : 0].isSelected = true;
+      _selectedYear = _userInfo['age'];
     });
   }
 }
