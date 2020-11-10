10eb593e4061cca9a15fabee9f7db42535a899ad
김민지 <kimin0412@gmail.com>
2020-11-10 15:46:26 +0900

    [UPDATE] Market Page

--- frontend/userApp/lib/market_page.dart
+++ frontend/userApp/lib/market_page.dart
@@ -40,79 +40,79 @@ class _MarketPage extends State<MarketPage> {
     return _userInfo == null
         ? Container()
         : Container(
-            child: Padding(
-                padding: EdgeInsets.all(8.0),
-                child: SafeArea(
-                  child: SingleChildScrollView(
-                      child: Center(
-                          child: Column(
-                    children: <Widget>[
-                      Card(
-                          elevation: 4.0,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20.0),
-                          ),
-                          child: InkWell(
-                            onTap: () => {
-                              // Navigator.push(
-                              // context,
-                              // MaterialPageRoute(builder: (context) => PointHistoryPage()),
-                              // ).then(refreshPage)
-                            },
-                            borderRadius: BorderRadius.circular(8.0),
-                            child: Container(
-                              height: 100,
-                              alignment: Alignment.center,
-                              margin: EdgeInsets.all(5),
-                              child: ListTile(
-                                leading: SizedBox(
-                                    width: 50.0,
-                                    height: 50.0,
-                                    child: Icon(Icons.account_circle,
-                                        color: Colors.black87, size: 50.0)),
-                                title: Text('${_userInfo['nickname']}님 어서오세요!',
-                                    style: TextStyle(
-                                        color: Colors.black87,
-                                        fontWeight: FontWeight.bold)),
-                                subtitle: Text('브론즈?',
-                                    style: TextStyle(color: Colors.black87)),
-                                trailing: Wrap(
-                                  spacing: 12,
-                                  children: <Widget>[
-                                    SizedBox(
+        child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: SafeArea(
+              child: SingleChildScrollView(
+                  child: Center(
+                      child: Column(
+                        children: <Widget>[
+                          Card(
+                              elevation: 4.0,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20.0),
+                              ),
+                              child: InkWell(
+                                onTap: () => {
+                                  // Navigator.push(
+                                  // context,
+                                  // MaterialPageRoute(builder: (context) => PointHistoryPage()),
+                                  // ).then(refreshPage)
+                                },
+                                borderRadius: BorderRadius.circular(8.0),
+                                child: Container(
+                                  height: 100,
+                                  alignment: Alignment.center,
+                                  margin: EdgeInsets.all(5),
+                                  child: ListTile(
+                                    leading: SizedBox(
                                         width: 50.0,
                                         height: 50.0,
-                                        child: Icon(Icons.card_giftcard,
-                                            color: Colors.black54, size: 50.0)),
-                                  ],
+                                        child: Icon(Icons.account_circle,
+                                            color: Colors.black, size: 50.0)),
+                                    title: Text('${_userInfo['nickname']}님 어서오세요!',
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.bold)),
+                                    subtitle: Text('브론즈?',
+                                        style: TextStyle(color: Colors.black)),
+                                    trailing: Wrap(
+                                      spacing: 12,
+                                      children: <Widget>[
+                                        SizedBox(
+                                            width: 50.0,
+                                            height: 50.0,
+                                            child: Icon(Icons.card_giftcard,
+                                                color: Colors.black54, size: 50.0)),
+                                      ],
+                                    ),
+                                  ),
                                 ),
-                              ),
-                            ),
-                          )), // 회원 정보 Card
-                      Padding(padding: EdgeInsets.all(8.0)),
-                      // ListView(
-                      //     scrollDirection: Axis.vertical,
-                      //     shrinkWrap: true,
-                      //     children: <Widget>[
-                      //       new GridView.count(
-                      //         scrollDirection: Axis.horizontal,
-                      //         //스크롤 방향 조절
-                      //         crossAxisSpacing: 10,
-                      //         mainAxisSpacing: 10,
-                      //         crossAxisCount: 6,
-                      //         //로우 혹은 컬럼수 조절 (필수값)
-                      //         children: List.generate(100, (index) {
-                      //           return Container(
-                      //             child: Text("Item $index",
-                      //                 style: Theme.of(context).textTheme.body1),
-                      //             color: Colors.green,
-                      //           );
-                      //         }),
-                      //       ),
-                      //     ]),
-                    ],
-                  ))),
-                )));
+                              )), // 회원 정보 Card
+                          Padding(padding: EdgeInsets.all(8.0)),
+                          // ListView(
+                          //     scrollDirection: Axis.vertical,
+                          //     shrinkWrap: true,
+                          //     children: <Widget>[
+                          //       new GridView.count(
+                          //         scrollDirection: Axis.horizontal,
+                          //         //스크롤 방향 조절
+                          //         crossAxisSpacing: 10,
+                          //         mainAxisSpacing: 10,
+                          //         crossAxisCount: 6,
+                          //         //로우 혹은 컬럼수 조절 (필수값)
+                          //         children: List.generate(100, (index) {
+                          //           return Container(
+                          //             child: Text("Item $index",
+                          //                 style: Theme.of(context).textTheme.body1),
+                          //             color: Colors.green,
+                          //           );
+                          //         }),
+                          //       ),
+                          //     ]),
+                        ],
+                      ))),
+            )));
   }
 
   void _getUserInfo() async {
